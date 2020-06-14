486f634a280eb405d054d3bb881a467b0365dd1f
deepthi2007 <deepthik5656@gmail.com>
2020-06-14 11:57:32 +0530

    vanishing pigs

--- Pig.js
+++ Pig.js
@@ -2,6 +2,19 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.visibilty = 255;
+  }
+  display(){
+    if(this.body.speed>10){
+    World.remove(world,this.body);
+    push();
+    this.visibilty=this.visibilty-5;
+    tint(255,this.visibilty);
+    image(this.image,this.body.position.x,this.body.position.y,this.width,this.height);
+    pop();
+    }else{
+      super.display();
+    }
   }
 
 };
